37dda8491a0ad512823c6ea54e7340cd05a24268
SVAKSHA <svaksha@gmail.com>
2014-11-28 18:20:07 +0530

    copied some packages... merging the repo might be faster?

--- aptget.sh
+++ aptget.sh
@@ -5,7 +5,19 @@ set -e
 cd
 sudo apt-get update -y
 sudo apt-get install -y \
+# C/C++ 
+    gcc \
+    gnu \
+    make \
+    gnu-make \
+    gfortran \
+    m4 \
+    patch \
+    clang \ 
+    g++ \
+# general
     curl \
+    wget \
     geany \
     meld \
     spyder \
@@ -13,6 +25,7 @@ sudo apt-get install -y \
     subversion \
     git \
     tig \
+    mercurial \
 ## PDF related packages
     flpsed \
     pdfjam \
@@ -21,13 +34,25 @@ sudo apt-get install -y \
 ## Skype alternative
     jitsi \
 ## HP printer stuff
-    hplip
-    mtink # http://xwtools.automatix.de/
+    hplip \
+    mtink \  # http://xwtools.automatix.de/
 # Reactivate HP LaserJet 1018/1020 after reloading paper
-    printer-driver-foo2zjs-common #20140209dfsg0-1ubuntu1    
-    
-    
-      
+    printer-driver-foo2zjs-common \   #20140209dfsg0-1ubuntu1    
+# smartctl-tools
+    smartctl \
+    smartmontools \
+# sensors package
+    lm-sensors \
+# more browsers
+    google-chrome-stable \    
+# Python stuff
+    pip \
+    pip-installer \
+    python-pip \
+    IPython \
+    Jinja2 \
+    Tornado \
+    pyzmq \
       
       
       
