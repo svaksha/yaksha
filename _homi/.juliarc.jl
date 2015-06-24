ce943e96a71a66ecde8d9acae3a93ef483f2e203
SVAKSHA ॥ स्वक्ष ॥ <svaksha@gmail.com>
2015-06-24 07:31:10 +0530

    moved screenrc from backup server

--- _homi/.juliarc.jl
+++ _homi/.juliarc.jl
@@ -22,13 +22,34 @@
 # to be executed when the Julia REPL starts up.
 #-------------------------------------------------------------------------------
 
-require Base
+using Base
 using IJulia
 #using HDF5
 using Docile
 
-Pkg.update()
+# Scripted updates to run only once in a day.
+# https://julia.readthedocs.org/en/latest/manual/packages/
+# http://docs.julialang.org/en/latest/stdlib/dates/
+#-------------------------------------------------------------------------------
+#=
+if isweekday == x->Dates.dayofweek(x) && Dates.tonext(isweekday, Date(2015,12,31)) 
+        Pkg.status()
+        Pkg.update()
+end
 
+# Scripted updates to run only on the first of each month for the year 2015.
+if first = length([Dates.firstdayofmonth(first+Dates.Month(1))
+    for first in Dates.Date(2015):Dates.Date(2015,12,31)]) == 365
+        do 
+        # Ensure the system is running the latest master branch.
+        Pkg.checkout("HDF5")     
+        Pkg.checkout("IJulia")
+        Pkg.checkout("Docile")
+        end
+     end
+end     
+=#
+ 
 # Setting options, https://julia.readthedocs.org/en/latest/manual/getting-started/
 # julia [options] [program] [args...]
 #-------------------------------------------------------------------------------
@@ -37,10 +58,10 @@ Pkg.update()
 using IniFile
 ini = Inifile()
 #read(ini, ENV["JULIA_APP_BOOTSTRAP"])
-#include_dir = get(ini, "data", "include_dir")
-#app_dir     = get(ini, "data", "app_dir")
+#include_dir      = get(ini, "data", "include_dir")
+#app_dir          = get(ini, "data", "app_dir")
 #util_include_dir = get(ini, "data", "util_include_dir")
-#data_dir     = get(ini, "data", "data_dir")
+#data_dir         = get(ini, "data", "data_dir")
 
 # Put on search path - CHD analysis, which is only necessary for CHD analysis
 #-------------------------------------------------------------------------------
@@ -52,3 +73,16 @@ ini = Inifile()
 #if isfile("$(ENV["HOME"])/.juliarc_5vksh.jl")
 #    include("$(ENV["HOME"])/.juliarc_5vksh.jl")
 #end
+
+# Using isinterative() to check if julia is run in REPL. 
+if isinteractive()
+        include("$(pwd)/.julia/v0.4/REPL/scripts/repl.jl:$PATH")
+end
+
+
+# Load the `start_tomo.jl` file in the current Tomography directory.
+#
+#if isfile("start_tomo.jl")
+#  include("start_tomo.jl")
+#end
+
