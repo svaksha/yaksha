ffe2a0aa464bb4e59dfddb5f177e0b9cd90f6bf4
SVAKSHA <svaksha@gmail.com>
2015-10-09 09:01:00 +0530

    updated (from LTP)

---
+++ yaksha-debos-backup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+################################################################################
+# File       : yaksha-debos-backup.py, from http://svaksha.github.io/yaksha
+# Description: Backup script for my system.
+# AUTHOR     : SVAKSHA, http://svaksha.github.io/yaksha
+# COPYRIGHT© : 2005-Now SVAKSHA <http://svaksha.com/pages/Bio> AllRightsReserved
+# DATES      : 2015sep21 - 2015sep28
+# LICENSE    : GNU AGPLv3 License <http://www.gnu.org/licenses/agpl.html>
+#              https://github.com/svaksha/yaksha/blob/master/LICENSE.md
+# This code is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. Permission is hereby granted, free of charge, to any
+# person obtaining a copy of this software, to deal in the Software without
+# restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, redistribute, and/or sell copies of the Software and use in
+# source form, with or without modification, and to permit persons to whom the
+# Software is furnished to do so as per the terms in the LICENSE.md file and
+# subject to the following conditions being met:
+# 1. ALL copies, forks and redistributions of source code AND/OR binary form
+#    must retain the above Copyright notice, the Licence file for source code
+#    (AGPLv3) along with this permission notice and list of conditions and the
+#    following disclaimer, in substantial portions of the Software.
+# 2. Neither the name of the COPYRIGHT HOLDER(S) nor the names of its
+#    contributors may be used to endorse or promote products derived from this
+#    software without specific prior written permission.
+################################################################################
+#
+
+import attic
+import rotate-backups
+import datetime
+import logging
+import os
+import shutil  # https://docs.python.org/3/library/shutil.html
+
+
+LOCAL_BACKUP_DIR = '/home/mom/2015-09-28-VID_{0}'
+#EXTERNAL_DRIVE_DIRECTORY = 'F:\\My Files\\Manifest_Destiny_Backups\\md_backup_{0}'
+
+def get_backup_directory(base_directory):
+    date = str(datetime.datetime.now())[:16]
+    date = date.replace(' ', '_').replace(':', '')
+    return base_directory.format(date)
+
+def copy_files(directory):
+    for file in os.listdir(LOCAL_BACKUP_DIR ):
+        file_path = os.path.join(LOCAL_BACKUP_DIR , file)
+        if os.path.isfile(file_path):
+            shutil.copy(file_path, directory)
+
+def perform_backup(base_directory):
+    backup_directory = get_backup_directory(base_directory)
+    os.makedirs(backup_directory)
+    copy_files(backup_directory)
+
+def main():
+    perform_backup(LOCAL_BACKUP_DIR)
+    #perform_backup(EXTERNAL_DRIVE_DIRECTORY)
+
+if __name__ == '__main__':
+    main()
