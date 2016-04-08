3932c110a20002047749f4edace197aa04c45dbc
SVAKSHA <svaksha@gmail.com>
2016-04-08 14:28:14 +0200

    improved the juliarc file

--- home/.juliarc-update.jl
+++ home/.juliarc-update.jl
@@ -1,32 +1,32 @@
 ################################################################################
 # FILE       : .juliarc-update.jl
-# DESCRIPTION: Configuration file for automagically updating Julia.
+# DESCRIPTION: Config file for automagically updating Julia.
 # AUTHOR     : SVAKSHA <http://svaksha.github.io/yaksha>
 # COPYRIGHT© : 2005-Now SVAKSHA <http://svaksha.com/pages/Bio> AllRightsReserved
-# LICENSE    : GNU AGPLv3 and subject to the terms and conditions in the LICENSE.md 
-#              (https://github.com/svaksha/yaksha/blob/master/LICENSE.md) file being met.
-# DATES      : Created:2013oct21 - Updated:2016mar16
+# LICENSE    : GNU AGPLv3 and subject to meeting all the terms in the LICENSE
+#              file: https://github.com/svaksha/yaksha/blob/master/LICENSE.md
+# DATES      : Created:2013oct21 - Updated:2016apr08
 ################################################################################
 #
 # Automagically update Julia 
-# Alternatively, USE THE "yaks-jl-update.sh" PROGRAM 
-# https://github.com/svaksha/yaksha/blob/master/yaks-jl-update.sh
+# Alternatively, USE THE "yaks-update-julia.sh" PROGRAM 
+# https://github.com/svaksha/yaksha/blob/master/yaks-update-julia.sh
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+push!(LOAD_PATH, ENV["HOME"]*"/.julia")
 
 # Update my Julia installation 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 Pkg.status()
 Pkg.update()
 
-## Load my .juliarc.jl each time the REPL runs
+# Load my .juliarc.jl for each REPL run, enable a per-directory startup file.
 if chomp(readstring(`pwd`)) != ENV["HOME"]
-    if isfile(".juliarc.jl"); require(".juliarc.jl")
+    if isfile(".juliarc.jl")
+        require(".juliarc.jl")
     end
 end
-
-push!(LOAD_PATH, ENV["HOME"]*"/.julia")
 
 # Optional additional initialization
 if isfile("$(ENV["HOME"])/.juliarc.jl"); include("$(ENV["HOME"])/.juliarc.jl"); end
 #if isfile("$(ENV["HOME"])/.juliarc_tomography.jl"); include("$(ENV["HOME"])/.juliarc_tomography.jl"); end
-
--- home/.juliarc.jl
+++ home/.juliarc.jl
@@ -3,26 +3,43 @@
 # DESCRIPTION: My Julia config file storing personal commands in homedir()
 # AUTHOR     : SVAKSHA <http://svaksha.github.io/yaksha>
 # COPYRIGHT© : 2005-Now SVAKSHA <http://svaksha.com/pages/Bio> AllRightsReserved
-# LICENSE    : GNU AGPLv3 and subject to the terms and conditions in the LICENSE.md 
-#              (https://github.com/svaksha/yaksha/blob/master/LICENSE.md) file being met.
-# DATES      : Created:2013oct01 - Updated:2016mar17
+# LICENSE    : GNU AGPLv3 and subject to meeting all the terms in the LICENSE
+#              file: https://github.com/svaksha/yaksha/blob/master/LICENSE.md
+# DATES      : Created:2013oct01 - Updated:2016apr08
 ################################################################################
 #
 # This file contains site-specific commands (EX. add directories to the LOAD_PATH,
 # to be executed when the Julia REPL starts up.
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+# Greet Users on startup.
+#-------------------------------------------------------------------------------
+println("|| नमस्ते ! स्वक्षंस्या सङ्गणकप्रक्रमम् स्वागतम  || Greetings! ")
+
+push!(LOAD_PATH, pwd())
+push!(LOAD_PATH, ENV["HOME"]*"/.julia")
+push!(LOAD_PATH, ENV["HOME"]*"/julia")
+push!(LOAD_PATH, ENV["HOME"]*"/dev-*")
+push!(LOAD_PATH, ENV["HOME"]*"/divya-*")
+
+#-------------------------------------------------------------------------------
+# Import these packages on REPL startup 
+#-------------------------------------------------------------------------------
 using Base
-#using PyCall
 
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Greet Users on startup
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-println("|| नमस्ते ! स्वक्षंस्या सङ्गणकप्रक्रमम् स्वागतम  || Greetings! Welcome to my Julia language programs ||")
+atreplinit() do repl
+    @eval using Gadfly
+    @eval using IJulia
+    # PyCall wont work in v0.5, Julia bug https://github.com/stevengj/PyCall.jl/issues/246 
+    @eval using PyCall
+end
 
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#julia --eval 'Pkg.add("IJulia")'
+#julia --eval 'Pkg.add("Gadfly")'
+
+#-------------------------------------------------------------------------------
 # add directories to the LOAD_PATH to be executed when the Julia REPL starts up.
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#-------------------------------------------------------------------------------
 
 #=
 # Load my .juliarc.jl for each REPL run, enable a per-directory startup file.
@@ -33,25 +50,58 @@ if chomp(readstring(`pwd`)) != ENV["HOME"]
 end
 =#
 
-push!(LOAD_PATH, ENV["HOME"]*"/.julia")
-push!(LOAD_PATH, ENV["HOME"]*"/julia")
-push!(LOAD_PATH, ENV["HOME"]*"/devya")
-push!(LOAD_PATH, ENV["HOME"]*"/divya")
-
 # for PRIVATE repos
 if isfile("$(ENV["HOME"])/.juliarc-yakshi.jl")
     include("$(ENV["HOME"])/.juliarc-yakshi.jl")
 end
 
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Import these packages on REPL startup 
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-atreplinit() do repl
-    @eval using Gadfly
-    @eval using IJulia
-    # PyCall wont work in v0.5, Julia bug https://github.com/stevengj/PyCall.jl/issues/246 
-    @eval using PyCall
+
+#-------------------------------------------------------------------------------
+# _init.jl 
+#-------------------------------------------------------------------------------
+#
+# If `_init.jl` exists, run at runtime.
+#-------------------------------------------------------------------------------
+if VERSION < v"0.5-"
+    if isinteractive() &isfile("_init.jl")
+      info("Found", joinpath(pwd(), "_init.jl"))
+      include(joinpath(pwd(),"_init.jl"))
+	end
+else 
+    atreplinit() do repl
+        if isfile("_init.jl")
+            info("Found", joinpath(pwd(),"_init.jl"))
+            include(joinpath(pwd(),"_init.jl"))
+        end
+	end
 end
 
-#julia --eval 'Pkg.add("IJulia")'
-#julia --eval 'Pkg.add("Gadfly")'
+# Macro to edit this file
+macro juliarc()
+    edit("~/.juliarc.jl")
+end
+
+# Methods macro
+macro methods(m)
+    @eval methods(m)
+end
+
+# Macro to run _init.jl if it exists
+#-------------------------------------------------------------------------------
+macro init()
+    if isfile("_init.jl")
+        info("Found", joinpath(pwd(), "_init.jl"))
+        include("_init.jl")
+    else
+        println("The file '_init.jl' is not found in current directory: $(pwd()). Create one instead? (y/n)")
+	r1 = readline(STDIN)
+	
+	if first(r1) == 'y'
+	    edit("_init.jl")
+	end
+  end
+end
+
+# REPL restart : http://docs.julialang.org/en/latest/manual/workflow-tips/#simplify-initialization
+#-------------------------------------------------------------------------------
+isinteractive() && isfile("_init.jl") && require("_init.jl")
